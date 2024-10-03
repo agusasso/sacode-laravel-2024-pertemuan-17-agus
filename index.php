fc59cbf12fe30be17f0115a5ae10baf974174f09
Agus Asso <agusasso22@gmail.com>
2024-10-03 20:06:36 +0900

    file

--- index.php
+++ index.php
@@ -15,13 +15,14 @@ class Mobil
         echo 'Mobil dinyalakan';
     }
 
-    public function cek($nama)
+    public function cek($nama, $ket)
     {
         echo 'Merek :' . $this->merk . '<br>';
         echo 'Model :' . $this->model . '<br>';
         echo 'Tahun :' . $this->tahun . '<br>';
         echo 'Warna :' . $this->warna . '<br>';
         echo 'Pemilik :' . $nama . '<br>';
+        echo 'Pemilik :' . $ket . '<br>';
     }
 
     public function stop()
@@ -30,12 +31,30 @@ class Mobil
     }
 }
 
+class Kendaraan extends Mobil
+{
+    public $jenis;
+
+    public function bergerak()
+    {
+        echo 'Kendaraan sedang bergerak';
+    }
+}
+
 // Objek
 $mobil = new Mobil();
+$kendaraan = new Kendaraan();
 $mobil->merk = 'Avansa';
 $mobil->model = 'Nova';
 $mobil->tahun = 2023;
 $mobil->warna = 'Putih';
 
 // Pangil Methode
-$mobil->cek("agus");
+
+$ket = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the  when an unknown printer took a galley of type and scrambled it to make a type specimen book.';
+
+
+$mobil->cek("agus", $ket);
+
+$kendaraan->merk = 'BMW';
+echo $kendaraan->merk;
